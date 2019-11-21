52517c3b0c25ec86d5d8817932be7a73b3f46d14
Sarah Skanes <agent154@abopu.com>
2019-11-20 22:57:11 -0330

    DataManager set to Java 1.8 compliance. Removed kotlin from DataManager

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    kotlin("jvm") version "1.3.60"
+//    kotlin("jvm") version "1.3.60"
 }
 
 repositories {
@@ -8,14 +8,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+//    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains:annotations:17.0.0")
     testImplementation("junit:junit:4.12")
 }
 
 group = "com.abopu"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 val developmentOnly by configurations.creating
 configurations {
